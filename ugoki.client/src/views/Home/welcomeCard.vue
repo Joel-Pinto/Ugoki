365910cb17ac5b61887ea5d45b3a638385c5d47c
Joel Pinto <geral.joelpinto@gmail.com>
2025-10-11 12:30:33 +0100

    fix: small border adjustment

--- ugoki.client/src/views/Home/welcomeCard.vue
+++ ugoki.client/src/views/Home/welcomeCard.vue
@@ -3,8 +3,8 @@
 </style>
 
 <template>
-    <div class="flex gap-1">
-        <div class="w-[50vw] bg-[var(--color-primary)] rounded-lg rounded-bl-none rounded-br-none p-4">
+    <div class="flex gap-2">
+        <div class="w-[50vw] bg-[var(--color-primary)] rounded-lg rounded-tr-[3rem] rounded-bl-none rounded-br-none p-4">
             <h1 class="text-[var(--color-username-home)] font-bold!">Good Morning, Joel Pinto!</h1>
         </div>
         <ActivityMonth />
